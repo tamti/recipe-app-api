03b755103ef1082523fcd0dd9d0f5c97497972fa
Tamta Kirtadze <tkirt11@freeuni.edu.ge>
2022-11-28 15:53:45 +0400

    add volumes, database, tests

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+ Calculaor functions
+
+"""
+
+
+def add(x, y):
+    """add numbers"""
+    return x + y
+
+def subtract(x, y):
+    """subtract x from y and return result."""
+
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,23 @@
+"""
+Sampla test
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTest(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        """Test adding numbers togetether."""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+
+    def test_subtract_numbers(self):
+        """Test subtract numbers """
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, -5)
